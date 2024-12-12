de4511a99b3a1a88a49ca59ee40448ef44663a3a
alamgirkabirpython <kabiralomgir720@gmail.com>
2024-12-13 01:39:34 +0600

    main.py

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
 import streamlit as st
-import torch
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from transformers import pipeline
 from datasets import load_dataset
 from io import BytesIO
 import soundfile as sf
 
-# Set device
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-
-# Load model and processor
-model_id = "openai/whisper-large-v3"
-
+# Load pipeline
 @st.cache_resource
-def load_model():
-    model = AutoModelForSpeechSeq2Seq.from_pretrained(
-        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
-    )
-    model.to(device)
-    processor = AutoProcessor.from_pretrained(model_id)
-    pipe = pipeline(
-        "automatic-speech-recognition",
-        model=model,
-        tokenizer=processor.tokenizer,
-        feature_extractor=processor.feature_extractor,
-        torch_dtype=torch_dtype,
-        device=device,
-    )
-    return pipe
+def load_pipeline():
+    return pipeline("automatic-speech-recognition", model="openai/whisper-large-v3")
 
-pipe = load_model()
+pipe = load_pipeline()
 
 # Streamlit app UI
 st.title("Speech-to-Text with Whisper")
